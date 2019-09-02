9c6cc08b2f727d430b0c1d27a1d1b045ee78da3d
Andrew Waller <andrewhwaller@gmail.com>
2019-09-02 00:47:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
+<<<<<<< HEAD
   return drivers.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase();
   });
@@ -15,5 +16,9 @@ function fuzzyMatch(drivers, name) {
 function matchName(drivers, name) {
   return drivers.filter(function (driver) {
     return driver.name.toLowerCase() === name.toLowerCase();
+=======
+  return drivers.filter(function (name) {
+    return name.toLowerCase() === name.toLowerCase();
+>>>>>>> 7a6b9a576f3d3c9e461feffc266c2f8ef60d173a
   });
 }
